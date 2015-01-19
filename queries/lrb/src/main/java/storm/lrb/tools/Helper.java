ef8023acdd67fec2517ab6d81bc63bc7d0058eb2
Karl-Philipp Richter <richtekp@informatik.hu-berlin.de>
2015-01-19 16:59:28 +0100

    removed unused string formatting methods in queries/lrb/src/main/java/storm/lrb/tools/Helper.java

--- queries/lrb/src/main/java/storm/lrb/tools/Helper.java
+++ queries/lrb/src/main/java/storm/lrb/tools/Helper.java
@@ -10,22 +10,6 @@ import org.apache.commons.lang3.tuple.Triple;
  */
 public class Helper {
 
-	public static String getXDfromXSD(String xsd) {
-		String[] tmp = xsd.split("-");
-		if (tmp.length == 3)
-			return tmp[0] + "-" + tmp[2];
-		else
-			return "";
-	}
-
-	public static Integer getDirFromXSD(Triple<Integer,Integer,Integer> xsd) {
-		return xsd.getRight();
-	}
-
-	public static Integer getXwayFromXSD(Triple<Integer,Integer,Integer> xsd) {
-		return xsd.getLeft();
-	}
-
 	public static String readable(List<String> fields) {
 		StringBuilder tmp = new StringBuilder();
 		for (Iterator iterator = fields.iterator(); iterator.hasNext();) {
