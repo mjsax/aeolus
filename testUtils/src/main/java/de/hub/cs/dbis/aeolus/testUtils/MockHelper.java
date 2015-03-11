f1afa3ed3cd4b2a9b74f22951472d3521cab253d
Karl-Philipp Richter <richtekp@informatik.hu-berlin.de>
2015-03-11 11:03:24 +0100

    moving reused code into MockHelper and therefore adjusting poms (intoducing dependencyManagement for mockito and removing version declaration in poms)

---
+++ testUtils/src/main/java/de/hub/cs/dbis/aeolus/testUtils/MockHelper.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 Humboldt-Universität zu Berlin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.hub.cs.dbis.aeolus.testUtils;
+
+/*
+ * #%L
+ * testUtils
+ * %%
+ * Copyright (C) 2014 - 2015 Humboldt-Universität zu Berlin
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+import backtype.storm.task.GeneralTopologyContext;
+import backtype.storm.tuple.Fields;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author richter
+ */
+public class MockHelper {
+
+	public static GeneralTopologyContext createTopologyContextMock() {
+		GeneralTopologyContext context = mock(GeneralTopologyContext.class);
+		when(context.getComponentOutputFields(null, null)).thenReturn(new Fields("dummy"));
+		return context;
+	}
+
+	private MockHelper() {
+	}
+}
