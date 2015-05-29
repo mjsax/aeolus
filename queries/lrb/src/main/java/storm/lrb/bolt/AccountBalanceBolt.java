5f4cd363266ff96713c66b14b64e81cf4f3cb435
Karl-Philipp Richter <richtekp@informatik.hu-berlin.de>
2015-05-29 16:31:17 +0200

    removed trailing whitespace which causes permantent trouble with git rebase (because git apply uses the patch or git patch command)

--- queries/lrb/src/main/java/storm/lrb/bolt/AccountBalanceBolt.java
+++ queries/lrb/src/main/java/storm/lrb/bolt/AccountBalanceBolt.java
@@ -6,9 +6,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,32 +43,32 @@ import storm.lrb.tools.Helper;
 
 
 /**
- * 
+ *
  * This bolt recieves the toll values assesed by the {@link TollNotificationBolt} and answers to account balance
  * queries. Therefore it processes the streams {@link TopologyControl#TOLL_ASSESSMENT_STREAM_ID} (for the former) and
  * {@link TopologyControl#ACCOUNT_BALANCE_REQUESTS_STREAM_ID} (for the latter)
- * 
+ *
  */
 public class AccountBalanceBolt extends BaseRichBolt {
-	
+
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = LoggerFactory.getLogger(AccountBalanceBolt.class);
-	
+
 	private OutputCollector collector;
 	public static final Fields FIELDS_INCOMING_TOLL_NOTIFICATION = TollNotificationBolt.FIELDS_OUTGOING_TOLL_NOTIFICATION;
 	public static final Fields FIELDS_INCOMING_TOLL_ASSESSMENT = TollNotificationBolt.FIELDS_OUTGOING_TOLL_ASSESSMENT;
-	
+
 	/**
 	 * Contains all vehicles and the accountinformation of the current day.
 	 */
 	private Map<Integer, VehicleAccount> allVehicles;
-	
+
 	@Override
 	public void prepare(@SuppressWarnings("rawtypes") Map conf, TopologyContext context, OutputCollector collector) {
 		this.collector = collector;
 		this.allVehicles = new HashMap<Integer, VehicleAccount>();
 	}
-	
+
 	@Override
 	public void execute(Tuple tuple) {
 		if(tuple.getSourceStreamId().equals(TopologyControl.TOLL_ASSESSMENT_STREAM_ID)) {
@@ -79,10 +79,10 @@ public class AccountBalanceBolt extends BaseRichBolt {
 			throw new RuntimeException(String.format("Errornous stream subscription. Please report a bug at %s",
 				Helper.ISSUE_REPORT_URL));
 		}
-		
+
 		this.collector.ack(tuple);
 	}
-	
+
 	private void updateBalance(Tuple tuple) {
 		Integer vid = tuple.getIntegerByField(TopologyControl.VEHICLE_ID_FIELD_NAME);
 		VehicleAccount account = this.allVehicles.get(vid);
@@ -96,12 +96,12 @@ public class AccountBalanceBolt extends BaseRichBolt {
 				.getOffset());
 		}
 	}
-	
+
 	private void getBalanceAndSend(Tuple tuple) {
 		AccountBalanceRequest bal = (AccountBalanceRequest)tuple
 			.getValueByField(TopologyControl.ACCOUNT_BALANCE_REQUEST_FIELD_NAME);
 		VehicleAccount account = this.allVehicles.get(bal.getVehicleIdentifier());
-		
+
 		if(account == null) {
 			LOG.debug("No account information available yet: at:" + bal.getTime() + " for request" + bal);
 			AccountBalance accountBalance = new AccountBalance(bal.getCreated(), bal.getQueryIdentifier(), 0, // balance
@@ -113,14 +113,14 @@ public class AccountBalanceBolt extends BaseRichBolt {
 			this.collector.emit(accountBalance);
 		}
 	}
-	
+
 	public Map<Integer, VehicleAccount> getAllVehicles() {
 		return allVehicles;
 	}
-	
+
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
 		declarer.declare(new Fields(TopologyControl.BALANCE_NOTIFICATION_REQUESTS_FIELD_NAME));
 	}
-	
+
 }
