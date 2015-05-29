0e7384460bc403921729e46d2b34825eb101f3af
Karl-Philipp Richter <richtekp@informatik.hu-berlin.de>
2015-05-29 17:33:21 +0200

    removed extranous header and added necessary comment in LastAverageSpeedBoltTest.java

--- queries/lrb/src/test/java/storm/lrb/bolt/LastAverageSpeedBoltTest.java
+++ queries/lrb/src/test/java/storm/lrb/bolt/LastAverageSpeedBoltTest.java
@@ -6,30 +6,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * #_
- */
-/*
- * Copyright 2015 Humboldt-Universität zu Berlin.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package storm.lrb.bolt;
 
@@ -63,38 +48,22 @@ import static org.powermock.api.mockito.PowerMockito.mock;
 import storm.lrb.TopologyControl;
 import storm.lrb.tools.Constants;
 
-
-
-
-
 /**
- * 
+ * Tests {@link LastAverageSpeedBolt} using mocks.
  * @author richter
  */
 public class LastAverageSpeedBoltTest {
-	
-	public LastAverageSpeedBoltTest() {}
-	
-	@BeforeClass
-	public static void setUpClass() {}
-	
-	@AfterClass
-	public static void tearDownClass() {}
-	
-	@Before
-	public void setUp() {}
-	
-	@After
-	public void tearDown() {}
-	
+
 	/**
-	 * Test of execute method, of class LastAverageSpeedBolt.
+	 * Test of execute method, of class LastAverageSpeedBolt. Tests the
+	 * calculation of the average of values passed in multiple tuples with a
+	 * certain minute value.
 	 */
 	@Test
 	public void testExecute() {
 		GeneralTopologyContext generalContextMock = mock(GeneralTopologyContext.class);
 		when(generalContextMock.getComponentId(anyInt())).thenReturn("componentID");
-		
+
 		when(generalContextMock.getComponentOutputFields(anyString(), anyString())).thenReturn(
 			new Fields(TopologyControl.MINUTE_FIELD_NAME, TopologyControl.XWAY_FIELD_NAME,
 				TopologyControl.SEGMENT_FIELD_NAME, TopologyControl.DIRECTION_FIELD_NAME,
@@ -106,11 +75,11 @@ public class LastAverageSpeedBoltTest {
 		TopologyContext contextMock = mock(TopologyContext.class);
 		when(contextMock.getComponentTasks(anyString())).thenReturn(taskMock);
 		when(contextMock.getThisTaskIndex()).thenReturn(0);
-		
+
 		instance.prepare(new Config(), contextMock, new OutputCollector(collector));
 		OutputFieldsDeclarer outputFieldsDeclarer = Mockito.mock(OutputFieldsDeclarer.class);
 		instance.declareOutputFields(outputFieldsDeclarer);
-		
+
 		// test last average speed calculation
 		int minuteOfTuple = 5;
 		int xway = 1;
@@ -141,9 +110,11 @@ public class LastAverageSpeedBoltTest {
 		int expectedMinute = minuteOfTuple+1;
 		assertEquals((int)expectedMinute, (int)resultMinute);
 	}
-	
+
 	/**
-	 * Test of calcLav method, of class LastAverageSpeedBolt.
+	 * Test of calcLav method, of class LastAverageSpeedBolt. Tests refusal of
+	 * illegal arguments as well as whether average is calculated correctly
+	 * ignoring the latest value.
 	 */
 	@Test
 	public void testCalcLav() {
@@ -165,5 +136,5 @@ public class LastAverageSpeedBoltTest {
 		result = instance.calcLav(speedValues, minute);
 		assertEquals(expResult, result, 0.0);
 	}
-	
+
 }
